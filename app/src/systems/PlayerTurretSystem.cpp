56527366b36bab78a99a84e1a51b7b4f59b52198
Jason Wood <jwood@startmail.com>
2026-08-28 16:06:54 +1000

    Undo accidentla reverts.

---
+++ app/src/systems/PlayerTurretSystem.cpp
@@ -0,0 +1,67 @@
+#include "PlayerTurretSystem.h"
+
+#include "components/PlayerTurretComponent.h"
+#include "engine/ecs/components/TransformComponent.h"
+#include "engine/input/InputComponent.h"
+#include "engine/camera/ActiveCameraComponent.h"
+#include "engine/math/util/ViewportUtil.h"
+
+namespace app
+{
+    ecs::SystemDeclaration PlayerTurretSystem::GetSystemDeclaration()
+    {
+        return ecs::SystemDeclaration()
+               .WithComponent<const PlayerTurretComponent>()
+               .WithComponent<ecs::components::TransformComponent>()
+               .WithSingletonComponent<input::InputComponent>()
+               .WithSingletonComponent<camera::ActiveCameraComponent>();
+    }
+
+    void PlayerTurretSystem::OnUpdate(const ecs::QueryResults& results)
+    {
+        ecs::ForEachArcheType(results, ecs::UpdateMode::SingleThreaded, false, [](const ecs::SystemUpdateData& updateData)
+        {
+            const auto& entities = updateData.GetEntities();
+            auto* transforms = updateData.GetComponentArray<ecs::components::TransformComponent>();
+            auto* input = updateData.GetSingletonComponent<input::InputComponent>();
+            auto* camera = updateData.GetSingletonComponent<camera::ActiveCameraComponent>();
+
+
+#if SPARK_EDITOR
+            const auto viewportRect = Application::Get()->GetEditor()->GetViewportRect();
+            const math::IntVec2 mousePos = editor::util::ScreenSpaceToGameViewportSpace( input->mouseX,  input->mouseY);
+#else
+            auto* window = Application::Get()->GetWindow();
+            const auto viewportRect = ui::Rect{ { 0, 0 },  { window->GetWidth(), window->GetHeight()} };
+            const math::IntVec2 mousePos = { input->mouseX, input->mouseY };
+#endif
+
+            math::Vec3 mouseWorldPos = math::util::ScreenToWorldPoint(mousePos
+                                                                      , camera->view
+                                                                      , camera->proj
+                                                                      , math::Vec4(0.f,
+                                                                          0.f,
+                                                                          static_cast<float>(viewportRect.size.x),
+                                                                          static_cast<float>(viewportRect.size.y)),
+                                                                      0.f);
+
+            const math::Vec3 direction = math::Normalized(camera->pos - mouseWorldPos);
+            mouseWorldPos = direction * camera->pos.y;
+
+            for (size_t i = 0; i < entities.size(); ++i)
+            {
+                auto& transform = transforms[i];
+
+                if (!transform.parent)
+                {
+                    continue;
+                }
+
+                math::Vec3 mouseLocalPos = { mouseWorldPos.z, transform.pos.y, mouseWorldPos.x };
+                math::Mat4 rotMat = math::LookAt(transform.pos, mouseLocalPos, math::Vec3(0.f, 1.f, 0.f));
+                auto inverseTransform = math::Inverse(transform.parent->worldTransform);
+                transform.rot = math::EulerFromMat4(inverseTransform * rotMat);
+            }
+        });
+    }
+}
